6fe928d38c9c82beb165c156b5af51aa113ec165
Joel Santos <joe@joesantos.io>
2025-09-02 21:26:43 +0100

    flat config

--- init.lua
+++ init.lua
@@ -318,8 +318,6 @@ require("blink.cmp").setup({
   -- sources = { default = { "lsp", "path", "buffer", "snippets" } },
 })
 
-require("telescopeconfig")
-
 require('harpoon-core').setup()
 
 ----------------------------------------------------
@@ -328,6 +326,110 @@ require('harpoon-core').setup()
 vim.lsp.enable({ "lua_ls", "rust_analyzer", "ts_ls", "gopls", "eslint" })
 -- NOTE: if you don't want to use custom files but lspconfig here is an example...
 -- vim.lsp.config("lua_ls", { settings = { Lua = { ... } } })
+
+----------------------------------------------------
+-- TELESCOPE
+
+local ts_actions = require("telescope.actions")
+
+-- method to quickly send to quickfix and replace
+local ts_search_replace = function(prompt_bufnr)
+  local action_state = require("telescope.actions.state")
+  local from_entry = require("telescope.from_entry")
+
+  -- DEV: copied from telescope codebase
+  local entry_to_qf = function(entry)
+    local text = entry.text
+
+    if not text then
+      if type(entry.value) == "table" then
+        text = entry.value.text
+      else
+        text = entry.value
+      end
+    end
+
+    return {
+      bufnr = entry.bufnr,
+      filename = from_entry.path(entry, false, false),
+      lnum = vim.F.if_nil(entry.lnum, 1),
+      col = vim.F.if_nil(entry.col, 1),
+      text = text,
+    }
+  end
+
+  -- DEV: copied from telescope codebase
+  local send_all_to_qf = function(prompt_bufnr, mode, target)
+    local picker = action_state.get_current_picker(prompt_bufnr)
+    local manager = picker.manager
+
+    local qf_entries = {}
+    for entry in manager:iter() do
+      table.insert(qf_entries, entry_to_qf(entry))
+    end
+
+    local prompt = picker:_get_prompt()
+    ts_actions.close(prompt_bufnr)
+
+    if target == "loclist" then
+      vim.fn.setloclist(picker.original_win_id, qf_entries, mode)
+    else
+      vim.fn.setqflist(qf_entries, mode)
+      local qf_title = string.format([[%s (%s)]], picker.prompt_title, prompt)
+      vim.fn.setqflist({}, "a", { title = qf_title })
+    end
+  end
+
+  send_all_to_qf(prompt_bufnr, " ")
+  vim.cmd("copen")
+
+  -- "cdo s/*/*/gc"
+end
+
+require("telescope").setup({
+  file_ignore_patterns = {
+    "node_modules",
+    "vendor",
+    ".git",
+    "bin",
+    "build",
+    "dist",
+  },
+  defaults = {
+    -- `hidden = true` is not supported in text grep commands.
+    -- vimgrep_arguments = vimgrep_arguments,
+
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+      n = {
+        ["<c-d>"] = require("telescope.actions").delete_buffer,
+      }, -- n
+      i = {
+        ["<C-h>"] = "which_key",
+        ["<c-d>"] = require("telescope.actions").delete_buffer,
+        ["<C-r>"] = ts_search_replace,
+        ["<C-p>"] = false,                           -- disable movement
+        ["<C-n>"] = false,                           -- disable movement
+        ["<C-j>"] = ts_actions.move_selection_next,     -- move down
+        ["<C-k>"] = ts_actions.move_selection_previous, -- move up
+        ["<C-o>"] = ts_actions.select_default,          -- enter
+      },
+    },
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--ignore-file",
+      ".gitignore",
+    },
+  },
+})
+require("telescope").load_extension("live_grep_args")
 
 ----------------------------------------------------
 -- THEME
